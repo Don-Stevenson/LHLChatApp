81ff947885d5e05abb74ad4e1c4e5493f32190b7
Don-Stevenson <dsteven1@gmail.com>
2021-03-26 14:22:33 -0400

    update to const

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const http = require('http');
+const http = require("http");
 const server = http.Server(app);
 
-app.use(express.static('client'));
+app.use(express.static("client"));
 
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
-io.on('connection', function (socket) {
-  socket.on('message', function (msg) {
-    io.emit('message', msg);
+io.on("connection", function (socket) {
+  socket.on("message", function (msg) {
+    io.emit("message", msg);
   });
 });
 
-
-server.listen(8080, function() {
-  console.log('Chat server running');
+server.listen(8080, function () {
+  console.log("Chat server running");
 });
