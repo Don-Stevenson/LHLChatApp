afe88dba7bc1b130ce6cb9c1dbcab5c7f31f78cd
Don-Stevenson <dsteven1@gmail.com>
2021-04-12 00:06:11 -0400

    add comments to explain

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
+// requirements and setup the app and the port
 const express = require("express");
+const io = require("socket.io")(server);
 const app = express();
-const PORT = 8080
+const PORT = 8080;
 
+// server setup
 const http = require("http");
 const server = http.Server(app);
 
+// configuring the express server
 app.use(express.static("client"));
 
-const io = require("socket.io")(server);
-
+// setting up the io websocket connection to send a message
 io.on("connection", function (socket) {
   socket.on("message", function (msg) {
     io.emit("message", msg);
   });
 });
 
+// showing that the server is listening
 server.listen(PORT, function () {
   console.log(`Chat server is running on ${PORT}`);
 });
